9e03ad03c187b3b14ed78e1cb3cbb907b19b64b3
Vijay <may31.py@gmail.com>
2020-09-24 12:23:18 +0530

    4 ms

---
+++ solutions/771. Jewels and Stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int cnt=0;
+        for(int i=0;i<J.length();i++){
+            for(int j=0;j<S.length();j++){
+                if(J[i]==S[j]) cnt+=1;
+            }
+        }
+        
+        return cnt;
+    }
+};
