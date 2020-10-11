2e56dec3816a1438335a3393a8033f64587b972c
Vijay <may31.py@gmail.com>
2020-10-11 12:22:57 +0530

    0 ms

--- solutions/509. Fibonacci Number.cpp
+++ solutions/509. Fibonacci Number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int fib(int N) {
-        int n= N;
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
-        
+        int arr[50];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<35;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[N];
         
     }
 };
